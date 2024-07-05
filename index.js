9ebd4117b000cd18dea6517663f77bdf305a3904
SAMIR GANIYEV <samirganiev777@gmail.com>
2024-07-05 23:40:22 +0500

    msin

---
+++ index.js
@@ -0,0 +1,23 @@
+import { students } from "./lib/db.js";
+import { reload } from "./lib/utils.js";
+import { Student } from "./components/Student.js";
+const main = document.querySelector('.place')
+const form = document.forms.namedItem('add_new')
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+    
+    const student = {
+        number: students.length,
+        name: new FormData(form).get('name'),
+        age: new FormData(form).get('age'), 
+    }
+    console.log(student);
+    if(student.name.length !== 0) {
+        students.push(student)
+        reload(students, main, Student)
+        return  
+    } 
+
+    alert('Заполните форму')
+}
